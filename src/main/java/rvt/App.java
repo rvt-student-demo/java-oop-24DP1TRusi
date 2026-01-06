f75206ab5caee90638da9b97fe1465d9f20d99c9
24DP1TRusi <A240167TR@rvt.lv>
2026-01-06 16:29:18 +0200

    Pasūtījumu vēsture, nepabeigts

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,26 @@
 package rvt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+        try (Scanner  reader = new Scanner(new File("data/data.csv"))) {
+            reader.nextLine();
+            while (reader.hasNextLine()) {
+                String row = reader.nextLine();
+                String[] parts = row.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+                int id = Integer.valueOf(parts[2]);
+                String email = parts[3];
+                String course = parts[4];
+                double gpa = Double.valueOf(parts[5]);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
