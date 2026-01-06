c1b1c04eca3945947338aa4903e6c8c87e1e5665
Tabita Rūsiņa <tabita.rusina@protonmail.com>
2026-01-06 22:52:41 +0200

    Pasūtījumu vēsture, pabeigts

--- src/main/java/rvt/Pasutijuma_vesture.java
+++ src/main/java/rvt/Pasutijuma_vesture.java
@@ -1,5 +1,33 @@
 package rvt;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Pasutijuma_vesture {
-    
+    public static void main(String[] args) {
+        try (Scanner  reader = new Scanner(new File("data/orders.csv"))) {
+            reader.nextLine();
+
+            double summa = 0;
+            while (reader.hasNextLine()) {
+                String row = reader.nextLine();
+                String[] parts = row.split(",");
+
+                int orderID = Integer.valueOf(parts[0]);
+                String customer = parts[1];
+                String product = parts[2];
+                int quantity = Integer.valueOf(parts[3]);
+                double price = Double.valueOf(parts[4]);
+                double kopa = price * quantity;
+
+                System.out.println("Pasūtījums #" + orderID + ": " + customer + " pasūtīja " + quantity + " x " + product + " (" + price + " EUR) -> Kopā: " + kopa + " EUR");
+                summa += kopa;
+            }
+            System.out.println("Kopējā pasūtījumu summa: " + summa + " EUR");
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
+
