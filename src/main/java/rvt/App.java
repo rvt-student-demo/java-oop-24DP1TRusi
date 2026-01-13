0caae338d8c76a40d338d40bb860d699cd437171
24DP1TRusi <A240167TR@rvt.lv>
2026-01-13 16:05:36 +0200

    To do list, nepabeigts

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,26 +1,17 @@
 package rvt;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        try (Scanner  reader = new Scanner(new File("data/data.csv"))) {
-            reader.nextLine();
-            while (reader.hasNextLine()) {
-                String row = reader.nextLine();
-                String[] parts = row.split(",");
-                String name = parts[0];
-                int age = Integer.valueOf(parts[1]);
-                int id = Integer.valueOf(parts[2]);
-                String email = parts[3];
-                String course = parts[4];
-                double gpa = Double.valueOf(parts[5]);
-            }
+        TodoList list = new TodoList();
+        list.add("read the course material");
+        list.add("watch the latest fool us");
+        list.add("take it easy");
 
-        } catch (FileNotFoundException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+        list.print();
+        list.remove(2);
+
+        System.out.println();
+        list.print();
     }
 }
