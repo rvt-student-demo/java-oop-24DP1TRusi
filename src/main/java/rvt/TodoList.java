bb4130ff0c211c5872d927ae28919ddf8ca1c339
Tabita Rūsiņa <tabita.rusina@protonmail.com>
2026-01-19 16:04:00 +0200

    To do list, ar failiem, nepabeigts

--- src/main/java/rvt/TodoList.java
+++ src/main/java/rvt/TodoList.java
@@ -1,22 +1,33 @@
 package rvt;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TodoList {
     private ArrayList<String> todo;
 
-    public TodoList () {
-        this.todo = new ArrayList<>();
-    }
+    public TodoList () {}
 
     public void add(String task) {
-        this.todo.add(task);
+        try (PrintWriter pWriter = new PrintWriter(new FileWriter("data/todo.csv", true))) {
+            pWriter.println(task);
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
     }
 
     public void print() {
-        for (int i = 0; i < todo.size(); i++) {
-            int num = i + 0;
-            System.out.println((num + 1) + ":" + todo.get(i));
+        try (Scanner  reader = new Scanner(new File("data/todo.csv"))) {
+            while (reader.hasNextLine()) {
+                String row = reader.nextLine();
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
     public void remove(int number) {
