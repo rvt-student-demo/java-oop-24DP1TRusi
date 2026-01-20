27e2ca8ae4eeeb41c51c487ca7983fcd0ad03c35
24DP1TRusi <A240167TR@rvt.lv>
2026-01-20 16:20:21 +0200

    To do list, pareizs, nepabeigts

--- src/main/java/rvt/TodoList.java
+++ src/main/java/rvt/TodoList.java
@@ -1,36 +1,62 @@
 package rvt;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class TodoList {
     private ArrayList<String> todo;
+    private final String filePath = "data/todo.csv";
 
-    public TodoList () {}
+    public TodoList () {
+        this.todo = new ArrayList<>();
+    }
+
+    private void loadFromFile() {
+        try (BufferedReader br = new BufferedReader(new FileReader("data/todo.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                this.todo.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
+    }
+
+    private int getListId() {
+        return todo.size();
+    }
 
     public void add(String task) {
-        try (PrintWriter pWriter = new PrintWriter(new FileWriter("data/todo.csv", true))) {
-            pWriter.println(task);
+        this.todo.add(task);
+
+        try (BufferedWriter pWriter = new BufferedWriter(new FileWriter("data/todo.csv", true))) {
+
         } catch (IOException ioe) {
             System.out.println(ioe.getMessage());
         }
     }
 
     public void print() {
-        try (Scanner  reader = new Scanner(new File("data/todo.csv"))) {
-            while (reader.hasNextLine()) {
-                String row = reader.nextLine();
-                System.out.println(row);
-            }
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+        for (int i = 0; i < todo.size(); i++) {
+            int num = i + 0;
+            System.out.println((num + 1) + ":" + todo.get(i));
         }
+
+        // try (Scanner  reader = new Scanner(new File("data/todo.csv"))) {
+        //     while (reader.hasNextLine()) {
+        //         String row = reader.nextLine();
+        //         System.out.println(row);
+        //     }
+        // } catch (Exception e) {
+        //     System.out.println("Error: " + e.getMessage());
+        // }
     }
-    public void remove(int number) {
-        this.todo.remove(number - 1);
+    public void remove(int id) {
+        this.todo.remove(id - 1);
     }
 }
