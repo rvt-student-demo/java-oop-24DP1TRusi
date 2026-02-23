2d89cef42c4e9f5c94cba29cc03e2e80f13c930e
Tabita Rūsiņa <tabita.rusina@protonmail.com>
2026-02-23 22:23:36 +0200

    ConsoleColor, pabeigts un pielietots Studentu reģistrācijā

--- src/main/java/rvt/student_registration/Show.java
+++ src/main/java/rvt/student_registration/Show.java
@@ -3,6 +3,7 @@ package rvt.student_registration;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import rvt.utils.ConsoleColors;
 
 public class Show {
 
@@ -11,10 +12,12 @@ public class Show {
             BufferedReader br = new BufferedReader(new FileReader("data/Students.csv"));
             String line = "+" + "-".repeat(20) + "+" + "-".repeat(20) + "+" + "-".repeat(30) + "+" + "-".repeat(15)
                     + "+" + "-".repeat(25) + "+";
-            System.out.println(line);
+
+            System.out.println(ConsoleColors.GREEN.code);
+            System.out.println(line + ConsoleColors.CYAN.code);
             System.out.printf("|%-20s|%-20s|%-30s|%-15s|%-25s|%n", "Vards", "Uzvards", "E-pasts", "Personal kods",
                     "Laiks un datums");
-            System.out.println(line);
+            System.out.println(ConsoleColors.GREEN.code + line + ConsoleColors.YELLOW.code);
 
             br.readLine();
             String linija;
@@ -23,7 +26,8 @@ public class Show {
                 System.out.printf("|%-20s|%-20s|%-30s|%-15s|%-25s|%n", parts[0], parts[1], parts[2], parts[3],
                         parts[4]);
             }
-            System.out.println(line);
+            System.out.println( ConsoleColors.GREEN.code + line);
+            System.out.println(ConsoleColors.RESET.code);
             br.close();
 
         } catch (IOException ioe) {
--- src/main/java/rvt/utils/ConsoleColors.java
+++ src/main/java/rvt/utils/ConsoleColors.java
@@ -1,10 +1,22 @@
 package rvt.utils;
 
 public enum ConsoleColors {
-    RED("\u001B[31m");
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    CYAN("\u001B[36m"),
+    YELLOW("\u001B[33m"),
+    BLACK("\u001B[30m"),
+    BLUE("\u001B[34m"),
+    MAGENTA("\u001B[35m"),
+    WHITE("\u001B[37m"),
+    BACKGROUND_WHITE("\u001B[47m"),
+    RESET("\u001B[0m");
 
 
-    final String code;
+
+
+
+    public final String code;
     ConsoleColors(String code) {
         this.code = code;
     }
